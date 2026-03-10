7817428cf98ef9ff2cae1a723a6d3df7ff7c9230
alikurb12 <gurbansahedovali@gmail.com>
2026-03-10 16:09:49 +0300

    add new model Project model

---
+++ app/models/project.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="projects")
+    tasks = relationship("Task", back_populates="project", cascade="all, delete-orphan")
